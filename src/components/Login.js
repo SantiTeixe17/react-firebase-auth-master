b706dae288034ebd473349295b574ed4daa9b9e6
SantiTeixe17 <santiteix@gmail.com>
2023-08-05 13:55:01 -0300

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { Alert } from "./Alert";
+
+export function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const { login, loginWithGoogle, resetPassword } = useAuth();
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await login(user.email, user.password, user.username);
+      window.location.replace('https://ecommercemundoelectro.mitiendanube.com/'); 
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleChange = ({ target: { value, name } }) =>
+    setUser({ ...user, [name]: value });
+
+  const handleGoogleSignin = async () => {
+    try {
+      await loginWithGoogle();
+      window.location.replace('https://ecommercemundoelectro.mitiendanube.com/'); 
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+    if (!user.email) return setError("Escribe un email para cambiar tu contraseña");
+    try {
+      await resetPassword(user.email);
+      setError('Te enviamos un email. Checkea tu buzón')
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="w-full max-w-xs m-auto">
+      {error && <Alert message={error} />}
+
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="username"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Usuario
+          </label>
+          <input
+            type="username"
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Usuario"
+          />
+        </div>
+        
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="tu@email"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Contraseña
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="*************"
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Login
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#!"
+            onClick={handleResetPassword}
+          >
+            Olvidaste tu contraseña?
+          </a>
+        </div>
+      </form>
+      <button
+        onClick={handleGoogleSignin}
+        className="bg-slate-50 hover:bg-slate-200 text-black shadow rounded border-2 border-gray-300 py-2 px-4 w-full"
+      >
+        Google Login
+      </button>
+      <p className="my-4 text-sl text-black flex justify-between px-3">
+        No tienes una cuenta?
+        <Link to="/register" className="text-blue-700 hover:text-blue-900">
+          Registrarse
+        </Link>
+      </p>
+    </div>
+  );
+}
